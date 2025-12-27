9b4349520588f6fcf94a1a5e95805f88192de738
ashiddiq40-lab <ashiddiq40@gmail.com>
2025-12-27 11:06:03 +0700

    Update script.js

--- UAS/script.js
+++ UAS/script.js
@@ -14,13 +14,13 @@ const dataUser = [
   }
 ];
 
-// CLASS (OOP)
+// CLASS OOP
 class Login {
   constructor(data) {
     this.data = data;
   }
 
-  cek(username, password) {
+  cekLogin(username, password) {
     return this.data.find(
       u => u.username === username && u.password === password
     );
@@ -30,21 +30,33 @@ class Login {
 // OBJECT
 const loginApp = new Login(dataUser);
 
-// FUNCTION LOGIN
+// LOGIN FUNCTION
 function login() {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
 
-  const user = loginApp.cek(username, password);
+  const user = loginApp.cekLogin(username, password);
 
-  if (user === undefined) {
+  if (!user) {
     alert("salah login");
   } else {
-    document.getElementById("hasil").innerHTML = `
-      <h3>Login Berhasil</h3>
-      <p>Nama : ${user.nama}</p>
-      <p>Username : ${user.username}</p>
-      <p>Level : ${user.level}</p>
-    `;
+    // sembunyikan login
+    document.getElementById("loginPage").style.display = "none";
+
+    // tampilkan info user
+    document.getElementById("infoPage").style.display = "block";
+
+    document.getElementById("infoNama").innerText = "Nama : " + user.nama;
+    document.getElementById("infoUsername").innerText = "Username : " + user.username;
+    document.getElementById("infoLevel").innerText = "Level : " + user.level;
   }
+}
+
+// LOGOUT
+function logout() {
+  document.getElementById("infoPage").style.display = "none";
+  document.getElementById("loginPage").style.display = "block";
+
+  document.getElementById("username").value = "";
+  document.getElementById("password").value = "";
 }
